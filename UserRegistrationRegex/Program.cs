38265843fc893993a83ab287e7dff5ea88c0ce18
Viney <vineykhaneja999@gmail.com>
2022-03-13 13:30:38 +0530

    [Viney] FEATURE ADDED: Practicing

--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace UserRegistrationRegex
 {
@@ -10,16 +11,16 @@ namespace UserRegistrationRegex
         #region Taking Inputs from Console & Storing them on the list.
         static void Main(string[] args)
         {
-            string chooseOptionForEnteringUserDetails = string.Empty;
-            List<UserDetails> usersList = new List<UserDetails>();
-            do
-            {
-                UserDetails userDetails = new UserDetails();
-                userDetails.ContactDetailsTakenFromConsole();
-                usersList.Add(userDetails);
-                Console.WriteLine("Do you want to enter more users details? \n\"Y\" or \"N\"");
-                chooseOptionForEnteringUserDetails = Console.ReadLine();
-            } while (chooseOptionForEnteringUserDetails.ToUpper() != "N");
+            //string chooseOptionForEnteringUserDetails = string.Empty;
+            //List<UserDetails> usersList = new List<UserDetails>();
+            //do
+            //{
+            //    UserDetails userDetails = new UserDetails();
+            //    userDetails.ContactDetailsTakenFromConsole();
+            //    usersList.Add(userDetails);
+            //    Console.WriteLine("Do you want to enter more users details? \n\"Y\" or \"N\"");
+            //    chooseOptionForEnteringUserDetails = Console.ReadLine();
+            //} while (chooseOptionForEnteringUserDetails.ToUpper() != "N");
         }
         #endregion
     }
